2047c072c9eff44096fa15b12ad5055a8f823ff3
Shubhi Yadav <shubhi.yadav.1234@gmail.com>
2021-09-19 02:51:45 +0530

    Added clue

---
+++ Space Stone/Assets/Scripts/Clue2Appear.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Clue2Appear : MonoBehaviour
+{
+
+    [SerializeField]
+    private Image clue2Img;
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            clue2Img.enabled = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            clue2Img.enabled = false;
+        }
+    }
+}
