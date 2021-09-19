97e442c76985e9ae3315b695077d34246d21400a
Ahana Vishwakarma <aha01na@gmail.com>
2021-09-19 14:48:11 +0530

    Door1 anim done

---
+++ Space Stone/Assets/Scripts/door1.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class door1 : MonoBehaviour
+{
+[SerializeField] private Animator MyAnimationController;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+            MyAnimationController.SetBool("playopen", true);
+    }
+}
--- Space Stone/Assets/Scripts/door4.cs
+++ Space Stone/Assets/Scripts/door4.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Doorpad : MonoBehaviour
+public class door4 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
