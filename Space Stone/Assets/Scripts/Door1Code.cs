05c9bf0a6b04b753f6cbc10b19739646804ebb31
Shubhi Yadav <shubhi.yadav.1234@gmail.com>
2021-09-22 18:42:48 +0530

    Added Pop Ups to the doors

---
+++ Space Stone/Assets/Scripts/Door1Code.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Door1Code : MonoBehaviour
+{
+
+
+    [SerializeField]
+    private GameObject Door1Img;
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+
+            ShowCursor();
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                HideCursor();
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+           
+            HideCursor();
+        }
+    }
+
+
+    public void ShowCursor()
+    {
+
+        Door1Img.SetActive(true);
+
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void HideCursor()
+    {
+
+        Door1Img.SetActive(false);
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
