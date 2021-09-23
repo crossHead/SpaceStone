b0cdf25f2f720e41090d5d55128be9509927d567
Ahana Vishwakarma <aha01na@gmail.com>
2021-09-23 15:05:41 +0530

    input ui

---
+++ Space Stone/Assets/Scripts/door2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class door2 : MonoBehaviour
+{
+    public InputField answer;
+
+    public GameObject door;
+    public GameObject door2img;
+    //[SerializeField] private Animator MyAnimationController;
+
+    public void onSubmit()
+    {
+        if(answer.text == "4")
+        {
+            door.SetActive(false);
+            door2img.SetActive(false);
+            Debug.Log("You did it!");
+        //    MyAnimationController.SetBool("play_anim", true);
+        }
+        else
+            Debug.Log("Try Again!");
+    }
+}
